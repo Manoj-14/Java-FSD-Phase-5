f964cc2dcf9a8292b4a8f0b4eb0a665b356d92c4
Manoj M <manumadhu1425@gmail.com>
2023-04-17 10:14:26 +0530

    Creating first component

---
+++ A01Intro/src/app/products/product-component/product-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product-component.component.html',
+  styleUrls: ['./product-component.component.css'],
+})
+export class ProductComponentComponent {}
