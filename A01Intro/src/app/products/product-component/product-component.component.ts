ceb22ae6af9fb44d563fb60f1bc90d7b3cee6968
Manoj M <manumadhu1425@gmail.com>
2023-04-17 10:33:37 +0530

    Product component

--- A01Intro/src/app/products/product-component/product-component.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-product',
-  templateUrl: './product-component.component.html',
-  styleUrls: ['./product-component.component.css'],
-})
-export class ProductComponentComponent {}
