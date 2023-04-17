2b346b65384e7aa8094277fc989840a486ccb95b
Manoj M <manumadhu1425@gmail.com>
2023-04-17 11:50:04 +0530

    property binding demo

--- A01Intro/src/app/server/server.component.ts
+++ A01Intro/src/app/server/server.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
 })
-export class ServerComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
+export class ServerComponent {
   serverId: number = 1;
   serverStatus: string = 'Offline';
 
