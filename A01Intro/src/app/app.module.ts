9c1e4c6a8a5c4710d7bbbc66423d5ca77443dbf1
Manoj M <manumadhu1425@gmail.com>
2023-04-17 10:43:42 +0530

    Component created manually

--- A01Intro/src/app/app.module.ts
+++ A01Intro/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent],
+  declarations: [AppComponent, ProductsComponent, WarningAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
