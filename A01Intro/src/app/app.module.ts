cb8714809f5165f10d46744fc8ab8f5b0bff9a70
Manoj M <manumadhu1425@gmail.com>
2023-04-20 10:03:16 +0530

    Adding Routes

--- A01Intro/src/app/app.module.ts
+++ A01Intro/src/app/app.module.ts
@@ -17,6 +17,20 @@ import { BetterHighlighterDirective } from './CustomDirectives/better-highlighte
 import { AccountsComponent } from './accounts/accounts.component';
 import { NewAccountComponent } from './accounts/new-account/new-account.component';
 import { AccountComponent } from './accounts/account/account.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ObservableDemoComponent } from './observable-demo/observable-demo.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Server', component: ServerComponent },
+  { path: 'Secret', component: SecretComponent },
+  { path: 'Success', component: SuccessAlertComponent },
+  { path: 'Warning', component: WarningAlertComponent },
+  { path: 'Product', component: ProductsComponent },
+  { path: 'Names', component: NamesComponent },
+  { path: 'Account', component: AccountsComponent },
+];
 
 @NgModule({
   declarations: [
@@ -35,8 +49,10 @@ import { AccountComponent } from './accounts/account/account.component';
     AccountsComponent,
     NewAccountComponent,
     AccountComponent,
+    HomeComponent,
+    ObservableDemoComponent,
   ],
-  imports: [BrowserModule, FormsModule], // Import FormModeule for two way binding
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)], // Import FormModeule for two way binding
   providers: [],
   bootstrap: [AppComponent],
 })
