47c17a2f545cc75e75111611f8514f8b1946c145
Manoj M <manumadhu1425@gmail.com>
2023-04-20 11:47:26 +0530

    Adding http boundry for formattin gusing pipe

--- A01Intro/src/app/observable-demo/observable-demo.component.ts
+++ A01Intro/src/app/observable-demo/observable-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, interval } from 'rxjs';
+import { Observable, Subscription, filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo',
@@ -13,40 +13,49 @@ export class ObservableDemoComponent implements OnInit, OnDestroy {
   private customObservableSubcription: Subscription;
 
   ngOnInit(): void {
-    this.intervalSubcription = interval(1000).subscribe((count) => {
-      console.log(count);
-    });
+    // this.intervalSubcription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
 
     const customObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
-        observer.next('A' + count);
+        observer.next(count);
         count++;
-        if (count == 6) {
+        if (count == 21) {
           observer.complete();
         }
-        if (count > 10) {
-          observer.error('Greater thsn 10');
-        }
-      }, 1000);
+        // if (count > 10) {
+        //   observer.error('Greater thsn 10');
+        // }
+      }, 500);
     });
 
-    this.customObservableSubcription = customObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (err) => {
-        alert(err.message);
-      },
-      () => {
-        //parameter my empty
-        console.log('Completed');
-      }
-    );
+    this.customObservableSubcription = customObservable
+      .pipe(
+        filter((data: number) => {
+          return data % 2 == 0;
+        }),
+        map((inpData: number) => {
+          return 'A ' + inpData;
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (err) => {
+          alert(err.message);
+        },
+        () => {
+          //parameter may empty
+          console.log('Completed');
+        }
+      );
   }
 
   ngOnDestroy(): void {
-    this.intervalSubcription.unsubscribe();
+    // this.intervalSubcription.unsubscribe();
     this.customObservableSubcription.unsubscribe();
   }
 }
